8f01dc0662ec41051788a7e2c6da588d03eccafe
wolodyx <wolodyx@yandex.ru>
2021-08-30 11:53:03 +0300

    Convert all md-files from report catalog to presentation files (html, pdf)

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 jupyter-book build --builder html .
-mkdir _build/html/report
+mkdir -p _build/html/report
 
 # Two formats of the presentation: html and pdf.
-npx @marp-team/marp-cli@latest report/intro.md -o _build/html/report/intro.html
-npx @marp-team/marp-cli@latest report/intro.md -o _build/html/report/intro.pdf
+for file in report/*.md
+do
+  file_html=`echo $file | sed 's/\.md/\.html/'`
+  npx @marp-team/marp-cli@latest $file -o _build/html/$file_html
+  file_pdf=`echo $file | sed 's/\.md/\.html/'`
+  npx @marp-team/marp-cli@latest $file -o _build/html/$file_pdf
+done
