05b9f40d3e71393ed42d5780abc6f06acbdffbe7
wolodyx <wolodyx@yandex.ru>
2021-08-30 11:27:00 +0300

    Generate intro.html & intro.pdf

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ jupyter-book build --builder html .
 mkdir _build/html/report
 
 # Two formats of the presentation: html and pdf.
-npx @marp-team/marp-cli@latest report/intro.md -o _build/html/report/intro-report.html
-npx @marp-team/marp-cli@latest report/intro.md -o _build/html/report/intro-report.pdf
+npx @marp-team/marp-cli@latest report/intro.md -o _build/html/report/intro.html
+npx @marp-team/marp-cli@latest report/intro.md -o _build/html/report/intro.pdf
