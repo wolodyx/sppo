06a172203592566fb58705e51a7efdb604cc8f54
wolodyx <wolodyx@yandex.ru>
2021-09-13 20:35:16 +0300

    file-system.html work piece

--- build.sh
+++ build.sh
@@ -1,18 +1,2 @@
 jupyter-book build --all --builder html .
-
-if command -v marp &> /dev/null
-then
-  marp_command="marp"
-else
-  marp_command="npx @marp-team/marp-cli@latest"
-fi
-
-# Two formats of the presentation: html and pdf.
-mkdir -p _build/html/report
-for file in report/*.md
-do
-  file_html=`echo $file | sed 's/\.md/\.html/'`
-  $marp_command $file -o _build/html/$file_html
-  file_pdf=`echo $file | sed 's/\.md/\.html/'`
-  $marp_command $file -o _build/html/$file_pdf
-done
+cp -r report/reveal.js _build/html/report
